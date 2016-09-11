577199482cecdf3262c3ef2515d155e2a4b6040d
Agentnick <nmagkoynis5@hotmail.com>
2016-09-11 18:27:22 +0300

    11092016

--- Chat_Client_Winform/Chat_Client_Winform/Properties/AssemblyInfo.cs
+++ Chat_Client_Winform/Chat_Client_Winform/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication1")]
+[assembly: AssemblyTitle("Chat_Client_Winform")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("ConsoleApplication1")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Chat_Client_Winform")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("994c9e11-82f8-4234-9560-66bcbaa1fb24")]
+[assembly: Guid("b8a5973d-fcc7-414b-9209-ccac59b9a215")]
 
 // Version information for an assembly consists of the following four values:
 //
